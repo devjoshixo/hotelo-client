7ba923b315abdb8e177fe29a22813d2ba1f74ec5
devjoshixo <djoshi923@gmail.com>
2024-02-17 19:00:43 +0530

    Lodging done

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Search from './Search/Search';
 import Banner from './Banner/Banner';
+import Lodging from './Lodging/Lodging';
 
 const Home = () => {
   return (
     <div>
       <Search />
       <Banner />
+      <Lodging />
     </div>
   );
 };
--- src/components/Home/Search/Search.jsx
+++ src/components/Home/Search/Search.jsx
@@ -1,6 +1,19 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import classes from './Search.module.css';
+
 const Search = () => {
+  const [details, setDetails] = useState({
+    destination: '',
+    checkInDate: new Date(),
+    checkOutDate: '',
+    travellers: { adults: 1, children: [] },
+    total: 1,
+  });
+
+  // const travellerClickHandler = () => {
+  //   return
+  // }
+
   return (
     <div className={classes.wrapper}>
       <header className={classes.section}>
@@ -8,7 +21,13 @@ const Search = () => {
         <div className={classes.searchbar}>
           <input type='text' className={classes.inputbox} />
           <input type='text' className={classes.inputbox} />
-          <input type='text' className={classes.inputbox} />
+          <div className={`${classes.inputbox} ${classes.passenger}`}>
+            <i className='fa-solid fa-user'></i>
+            <div>
+              <h5>Travellers</h5>
+              <p></p>
+            </div>
+          </div>
           <button>Search</button>
         </div>
       </header>
