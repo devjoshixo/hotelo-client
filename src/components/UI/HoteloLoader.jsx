7d22fe8d569301bf3381dcb2e87eff8266750893
devjoshixo <djoshi923@gmail.com>
2024-03-27 16:52:39 +0530

    Deployed from fast deployed

--- src/components/UI/HoteloLoader.jsx
+++ src/components/UI/HoteloLoader.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import classes from './Hotelo.module.css';
+import logo from '../../assets/hotel-solid.svg';
 
 const HoteloLoader = () => {
   return (
     <div className={classes.wrapper}>
       <div className={classes.header}>
         <div className={classes.heading}>
-          <i className={`fa-solid fa-hotel ` + classes.icon}></i>
-          <h1 className={classes.heading}>Hotelo</h1>
+          {/* <i className={`fa-solid fa-hotel ` + classes.icon}></i> */}
+          <div className={classes.icon}>
+            <img src={logo} alt='' className={classes.icon} />
+          </div>
+          <h2 className={classes.heading}>Hotelo</h2>
           <p className={classes.com}>.com</p>
         </div>
         <div className={classes.loader}></div>
