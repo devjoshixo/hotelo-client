911ddef84f089128e0a70b9fe10040ffd4999fae
devjoshixo <djoshi923@gmail.com>
2024-05-05 15:21:06 +0530

    Deployed from fast deployed

--- src/api/getHotelDetails.js
+++ src/api/getHotelDetails.js
@@ -1,4 +1,4 @@
-const getHotelDetails = async () => {
+const getHotelDetails = async (id) => {
   var requestOptions = {
     method: 'GET',
     headers: { 'api-key': import.meta.env.VITE_REACT_APP_API_KEY },
@@ -6,7 +6,9 @@ const getHotelDetails = async () => {
   };
 
   const data = await fetch(
-    import.meta.env.VITE_REACT_APP_API_URL + '/api/hotels/getHotelDetails',
+    import.meta.env.VITE_REACT_APP_API_URL +
+      '/api/hotels/getHotelDetails?id=' +
+      id,
     requestOptions
   );
   const result = await data.json();
--- src/components/Hotel/Hotel.jsx
+++ src/components/Hotel/Hotel.jsx
@@ -1,7 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
+import uniqid from 'uniqid';
+import getHotelDetails from '../../api/getHotelDetails';
 
 const Hotel = () => {
-  return <div>Hotel</div>;
+  const [hotel, setHotel] = useState();
+  const location = useLocation();
+
+  useEffect(() => {
+    const getHotel = async () => {
+      const hotel = await getHotelDetails(location.pathname.split('/')[2]);
+      setHotel(hotel.property);
+    };
+
+    getHotel();
+    // console.log(hotel.propertyGallery.images);
+  }, []);
+
+  return (
+    <div>
+      {hotel && (
+        <div>
+          {hotel.propertyGallery.images.map((image) => {
+            return <img src={image.image.url} alt='' key={uniqid()} />;
+          })}
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Hotel;
