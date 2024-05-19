10561ca022ed1e42a8c6b1f633b95c609ee9ba27
devjoshixo <djoshi923@gmail.com>
2024-05-19 14:01:23 +0530

    Deployed from fast deployed

--- src/components/Hotel/Details/Area/Area.jsx
+++ src/components/Hotel/Details/Area/Area.jsx
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
+import ReactCrop from 'react-image-crop';
+import 'react-image-crop/dist/ReactCrop.css';
 import classes from './Area.module.css';
 
 const Area = (props) => {
+  const [crop, setCrop] = useState({
+    unit: '%', // Can be 'px' or '%'
+    x: 25,
+    y: 25,
+    width: 50,
+    height: 50,
+  });
   return (
     <div className={classes.wrapper}>
       <h3 className={classes.header}>Enjoy the area</h3>
       <div className={classes.imageWrapper}>
         <div className={classes.image}>
-          <img src={props.location.staticImage.url + '&size=375x250'} alt='' />
+          <ReactCrop crop={crop} onChange={(c) => setCrop(c)}>
+            <img src={props.location.staticImage.url} alt='' />
+          </ReactCrop>
         </div>
       </div>
     </div>
