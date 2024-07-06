44278086c83188f311727f47047ae7aa2feb2435
devjoshixo <djoshi923@gmail.com>
2024-07-06 21:54:37 +0530

    'Room details 90% done'

--- src/components/Hotel/Rooms/RoomItem/RoomItem.jsx
+++ src/components/Hotel/Rooms/RoomItem/RoomItem.jsx
@@ -5,14 +5,22 @@ import Image from './Sections/Image/Image';
 import Breakfast from './Sections/Breakfast/Breakfast';
 import Price from './Sections/Price/Price';
 
+const ICONS = {
+  done: <i class='fa-solid fa-check'></i>,
+  people: <i class='fa-solid fa-users'></i>,
+  wifi: <i class='fa-solid fa-wifi'></i>,
+  local_parking: <i class='fa-solid fa-car'></i>,
+  dimension: <i class='fa-solid fa-chart-area'></i>,
+  location_city: <i class='fa-solid fa-city'></i>,
+  bed: <i class='fa-solid fa-bed'></i>,
+};
+
 const RoomItem = (props) => {
   const [breakfast, setBreakfast] = useState(0);
 
   const toggleBreakfast = (event) => {
     setBreakfast(event.target.value);
   };
-  console.log(breakfast);
-  console.log(!breakfast == true ? '1' : '0');
 
   return (
     <div className='w-[20rem] h-[50rem] bg-[white] rounded-[20px] border flex flex-col justify-between pb-3 border-[#64646441]'>
@@ -23,11 +31,20 @@ const RoomItem = (props) => {
           <div className='flex flex-col mt-4 gap-1'>
             {props.room.features.map((feature) => {
               return (
-                <div key={uniqid()}>
+                <div className='flex items-center gap-2' key={uniqid()}>
+                  {feature.icon ? (
+                    ICONS[feature.icon.id]
+                  ) : (
+                    <img src={feature.graphic.url.value} className='w-5 h-5' />
+                  )}
                   <p>{feature.text}</p>
                 </div>
               );
             })}
+            <div className='text-[blue] text-base flex items-center gap-1 '>
+              <p className='hover:underline'>More details</p>
+              <i className='fa-solid fa-angle-right scale-[0.8]'></i>
+            </div>
           </div>
         </div>
         {props.room.primarySelections[0].secondarySelections ? (
